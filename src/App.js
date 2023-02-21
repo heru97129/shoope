aeefced2324f9f9f61b3d4cd0a1cee393ff8ddc1
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-02-21 15:50:17 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useSelector ,useDispatch} from 'react-redux'
 import './App.css';
+import { addUsers,deleteUsers } from './features/Users';
+
 
 function App() {
+  const dispatch = useDispatch()
+  const userList = useSelector((state)=> state.users.value)
+  let [name,setName] = useState("")
+  let [username,setUsername] = useState("")
+
+
+  console.log(userList)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {" "}
+    <div className="addUser">
+      <input
+        type="text"
+        placeholder="Name..."
+        onChange={(e) => setName(e.target.value) }
+      />
+      <input
+        type="text"
+        placeholder="Username..."
+        onChange={(e) => setUsername(e.target.value) }
+
+      />
+      <button
+      onClick={()=> dispatch(addUsers({id:(userList.length) +1,name,username}))}
+      >
+        {" "}
+        Add User
+      </button>
     </div>
+    <div className="displayUsers">
+      {userList.map((user) => {
+        console.log(user.id)
+        return (
+          <div>
+            <h1> {user.name}</h1>
+            <h1> {user.username}</h1>
+            <input
+              type="text"
+              placeholder="New Username..."
+      
+            />
+            <button
+     
+            >
+              {" "}
+              Update Username
+            </button>
+            <button
+             onClick={()=> dispatch(deleteUsers(user.id))}
+            >
+              Delete User
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  </div>
   );
 }
 
