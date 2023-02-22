b1288a531704c460a88460c79659179676188d93
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-02-22 15:57:45 +0100

    footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from './footer.module.scss'
+export function Footer({}) {
+  return (
+    <div className={styles['footer']}>
+      <div className={styles['footer__terms']}>
+        <div className={styles["terms-link"]}>
+          <ul>
+            <li>CONTACT</li>
+            <li>TERMS OF SERVICES</li>
+            <li>SHIPPING OF SERVICES</li>
+          </ul>
+        </div>
+
+        <div className="terms-newsletter">
+          <input placeholder="Give an email, get the newsletter." />
+        </div>
+      </div>
+      <div className={styles['footer__policy']}>
+        <p>© 2021 Shelly. Terms of use and privacy policy.</p>
+        <div className={styles['reseau']}>
+          <ul>
+            <li>
+              <i class="fa-brands fa-linkedin-in"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-facebook-f"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-instagram"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-twitter"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Menu } from '../menu/Menu';
+import { Footer } from "../footer/Footer";
+import React from "react";
+import { Menu } from "../menu/Menu";
 
-const Layout =({children}) =>{
-    return(
-        <>
-        <Menu />
-        <main>
-            
-            {children}
-        
-        </main>
-        </>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Menu />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
