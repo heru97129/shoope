65256cd80ed99d6462a3720a2da6c940208a89a5
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-03-21 19:15:51 +0100

    state data product

--- src/features/Users.js
+++ src/features/Users.js
@@ -1,117 +1,113 @@
-import { objectMethod } from '@babel/types';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { prettyDOM } from '@testing-library/react';
+import {objectMethod} from '@babel/types';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {prettyDOM} from '@testing-library/react';
 import axios from 'axios';
-import { act } from 'react-dom/test-utils';
+import {act} from 'react-dom/test-utils';
 let compteById = {};
 let tab = []
-let check ={}
+let check = {}
 let compte = 0
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-  const response = await axios.get("https://fakestoreapi.com/products");
-  return response.data;
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() => {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    return response.data;
 });
 const postsSlice = createSlice({
-  name: 'posts',
-  initialState: {
-    data: [],
-    count:0,
-    countProduct:[],
-    order:[],
-    status: 'idle',
-    error: null,
-  },
-  reducers: {
-    order:(state,action)=>{
-      if(action.payload === '+'){
-        state.count = state.count + 1
-
-      }
-      
-      else if (action.payload === '-' && state.count != 0){
-        state.count = state.count - 1
-
-      }
-
+    name: 'posts',
+    initialState: {
+        data: [],
+        count: 0,
+        countProduct: [],
+        order: [],
+        status: 'idle',
+        error: null
     },
-    addprod:(state,action)=>{
-      //  console.log(action.payload,'count')
-       let data = [...action.payload]
-       let tab = []
-       let num = 0
-       data.filter((prod,i)=>{ 
-          if(Number(prod.id) === Number(action.payload[i].id)){
-             if(prod.change){
-              tab[num]= prod
+    reducers: {
+        order: (state, action) => {
+            if (action.payload === '+') {
+                state.count = state.count + 1
 
-             }
-            tab[num]= prod
-          
-          }
-   
- 
-        })
-       console.log(tab,'i')
-       state.countProduct = tab
-    },
-    neworder:(state,action)=>{
-   
-       let data = action.payload
-             
-          let newData = {...data[0]}
-     if(!check[data[0].id]){
-      compte = 1
-          check[data[0].id] = 'test'
-          console.log(check,newData,'newdata')
-          newData.compte = compte
-          tab.push(newData)
+            } else if (action.payload === '-' && state.count != 0) {
+                state.count = state.count - 1
 
-       
-        }else{
+            }
 
-          let newItem = {}
-            tab.forEach((item,i)=>{
-              let {id } = item
-              newItem = {...item}
-              if(id === data[0].id){
-         
-          
-              console.log('yoooooooo',compte)
-    
-                newItem.compte += 1
-                console.log(newItem)
-                tab.slice(i,1)
-                tab[i] = newItem
-              }
+        },
+        addprod: (state, action) => {
+            //  console.log(action.payload,'count')
+            let data = [...action.payload]
+            let tab = []
+            let num = 0
+            data.filter((prod, i) => {
+                if (Number(prod.id) === Number(action.payload[i].id)) {
+                    if (prod.change) {
+                        tab[num] = prod
+
+                    }
+                    tab[num] = prod
+
+                }
+
             })
 
+            state.countProduct = tab
+        },
+        neworder: (state, action) => {
 
+            let data = action.payload
+
+            let newData = {
+                ...data[0]
+            }
+            if (!check[data[0].id]) {
+                compte = 1
+                check[data[0].id] = 'test'
+
+                newData.compte = compte
+                tab.push(newData)
+
+            } else {
+
+                let newItem = {}
+                tab.forEach((item, i) => {
+                    let {id} = item
+                    newItem = {
+                        ...item
+                    }
+                    if (id === data[0].id) {
+
+                        newItem.compte += 1
+
+                        tab.slice(i, 1)
+                        tab[i] = newItem
+                    }
+                })
+
+            }
+
+            state.order = [...tab]
         }
 
-console.log(tab)
-        
-      state.order = [...tab]
-      }
-        
-     
-        
-  },
-  extraReducers: {
-    [fetchPosts.pending]: (state) => {
-      state.status = 'loading';
     },
-    [fetchPosts.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.data = action.payload;
-    },
-    [fetchPosts.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    },
-  },
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.status = 'succeeded';
+            state.data = action.payload;
+        },
+        [fetchPosts.rejected]: (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+        }
+    }
 });
 
 export const selectAllPosts = (state) => state.posts.data;
-export const {order,neworder,addprod} = postsSlice.actions;
+export const {
+    order,
+    neworder,
+    addprod
+} = postsSlice.actions;
 
 export default postsSlice.reducer;
--- src/pages/product/articles/Products.js
+++ src/pages/product/articles/Products.js
@@ -1,204 +1,238 @@
-import { Desciption } from "../../../components/description/Desciption";
+import {Desciption} from "../../../components/description/Desciption";
 import React from "react";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import styles from "./product.module.scss";
 import Layout from "../../../components/layout/Layout";
-import { useSelector, useDispatch,dispatch } from "react-redux";
-import { order, selectAllPosts,neworder,addprod } from "../../../features/Users";
-import { fetchPosts } from "../../../features/Users";
-import {
-  Link,
-  useParams,
-  useLocation,
-  useHistory,
-  useRouteMatch,
-} from "react-router-dom";
+import {useSelector, useDispatch, dispatch} from "react-redux";
+import {order, selectAllPosts, neworder, addprod} from "../../../features/Users";
+import {fetchPosts} from "../../../features/Users";
+import {Link, useParams, useLocation, useHistory, useRouteMatch} from "react-router-dom";
 import AddInformations from "../../../components/AddInformations/AddInformations";
 import Review from "../../../components/review/Review";
 
 let tb = []
 
 function Products(props) {
-  
-  const dispatch = useDispatch();
-  const post = useSelector(selectAllPosts);
-  let newShit = useSelector(state => state.posts.order)
-  
-  const router = useLocation();
-  let [data, setdata] = useState();
-  let [categories, setcategories] = useState();
-  let [items, setitems] = useState(1);
-  let [count, setcount] = useState(1);
-  let [update, setupdate] = useState(false);
-  let [nombre, setnombre] = useState(0);
-  let [newdata, setnewdata] = useState();
 
+    const dispatch = useDispatch();
+    const post = useSelector(selectAllPosts);
+    let newShit = useSelector(state => state.posts.order)
 
-  function ItemsSwitch(num) {
-    setitems(num)
-  }
+    const router = useLocation();
+    let [data,
+        setdata] = useState();
+    let [categories,
+        setcategories] = useState();
+    let [items,
+        setitems] = useState(1);
+    let [count,
+        setcount] = useState(1);
+    let [update,
+        setupdate] = useState(false);
+    let [nombre,
+        setnombre] = useState(0);
+    let [newdata,
+        setnewdata] = useState();
 
-  function NumberofItems(e){
-   let sign = e.target.innerText
+    function ItemsSwitch(num) {
+        setitems(num)
+    }
 
-   if(sign === '+'){
-setcount(count + 1 )
+    function NumberofItems(e) {
+        let sign = e.target.innerText
 
-    dispatch(order('+'))
-    dispatch(neworder(data))
-tb.push({id:data.map(st=> st.id).join(''),count:count,change:update})
+        if (sign === '+') {
+            
+            setcount(count + 1)
+            
+            dispatch(order('+'))
+            dispatch(neworder(data))
 
-    dispatch(addprod(tb))
-setupdate(false)
-   }
-   
-   else if(sign === '-' ){
-    dispatch(order('-'))
-    setcount(count - 1)
+            tb.push({
+                id: data
+                    .map(st => st.id)
+                    .join(''),
+                count: count,
+                change: update
+            })
+            
+            dispatch(addprod(tb))
+            setupdate(false)
+            
+            console.log('TB DANS PLUS',tb,data,'data',newShit,'Ns')
+        } else if (sign === '-') {
+            dispatch(order('-'))
+            setcount(count - 1)
 
-   }
-  }
+        }
+    }
 
+    useEffect(() => {
+        setcount(1)
+        setupdate(true)
+        dispatch(fetchPosts("fulfilled"));
+        if (post.length > 0) {
+          
+            let catego;
+            let copy = post.filter((product) => {
+                if (product.id === Number(router.pathname.substring(9))) {
+                    catego = product.category;
 
+                }
 
-  useEffect(() => {
-    setcount(1)
-    setupdate(true)
-    dispatch(fetchPosts("fulfilled"));
-    if (post.length > 0) {
-      let catego;
-      let copy = post.filter((product) => {
-        if (product.id === Number(router.pathname.substring(9))) {
-          catego = product.category;
-     
+                setnombre(0)
+
+                return product.id === Number(router.pathname.substring(9));
+            });
+            let copyCategories = post.filter((product) => product.category === catego);
+            copyCategories = copyCategories.slice(0, 3);
+
+            setcategories(copyCategories);
+            setdata(copy)
+            setnewdata(copy)
+            if(data){
+              tb =  [...tb,{id:copy[0].id.toString(),count:count,update:update}]
+              newShit = []
+              console.log(newShit,'new article')
+            }
+         
         }
 
-       
+    }, [
+        post.length > 0,
+        router.pathname
+    ]);
 
-        return product.id === Number(router.pathname.substring(9));
-      });
-      let copyCategories = post.filter(
-
-        (product) => product.category === catego
-        
-      );
-      copyCategories = copyCategories.slice(0, 3);
+    function Compteproduct() {
+      // console.log(newShit.length > 0, 'testoooo', newShit, data, 'id data',)
+     if(nombre === 0) newShit = []
       
-      setcategories(copyCategories);
-     setdata(copy)
-     setnewdata(copy)
-    
+        if (newShit.length > 0) {
+            let compte = newShit.find(news => news.id === data[0].id)
+            let numberprod = compte.compte
+
+            setnombre(numberprod + 1)
+        } else {
+            setnombre(1)
+
+        }
     }
-   
-  }, [post.length > 0,router.pathname,newShit.length > 0]);
-  console.log(newdata,'new')
-
-  function Compteproduct(){
 
 
-    if(newShit.length > 0){
-     let compte = newShit.find(news => news.id === data[0].id)
-     let numberprod = compte.compte
-     console.log(numberprod,compte,'number')
-     setnombre( numberprod + 1)
-    }
-  }
-  return (
-    <Layout>
-      <div className={styles["prod"]}>
-        <div className={styles["prod__describ"]}>
-          {newdata &&
-            newdata.map((product) => {
-              let { category, description, id, image, price, title,compte } = product;
-         
-              
-              return (
-                <div className={styles["container"]} key={id}>
-                  <div className={styles["product-pic"]}>
-                    <div className={styles["grid-pic"]}>
-                      <div>
-                        <img src={image} />
-                      </div>
-                      <div>
-                        <img src={image} />
-                      </div>
-                      <div>
-                        <img src={image} />
-                      </div>
-                      <div>
-                        <img src={image} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles["product-infos"]}>
-                    <h2>{title}</h2>
-                    <p className={styles["product-infos__price"]}>$ {price}</p>
-                    <p>{description}</p>
-                    <div className={styles["product-infos__btn"]}>
-                      <div className={styles["btn-add"]}>
-                        <div>
-                          <p onClick={NumberofItems}>-</p>
-                        </div>
-                        <div>
-                          <p>{nombre}</p>
-                        </div>
-                        <div>
-                          <p onClick={(e)=>{ 
-                          NumberofItems(e)
-                           Compteproduct()
-                          }
-                          }>+</p>
-                        </div>
-                      </div>
 
-                      <div className={styles["btn-add"]}>
-                        <p>ADD TO CART</p>
-                      </div>
-                    </div>
-                    <div className={styles["reseau"]}>
-                      <ul>
-                        <li>
-                          <i className="fa-regular fa-heart"></i>
-                        </li>
-                        <li>|</li>
-                        <li>
-                          <i className="fa-regular fa-envelope"></i>
-                        </li>
-                        <li>
-                          <i className="fa-brands fa-facebook-f"></i>
-                        </li>
-                        <li>
-                          <i className="fa-brands fa-instagram"></i>
-                        </li>
-                        <li>
-                          <i className="fa-brands fa-twitter"></i>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+    return (
+        <Layout>
+            <div className={styles["prod"]}>
+                <div className={styles["prod__describ"]}>
+                    {newdata && newdata.map((product) => {
+                        let {
+                            category,
+                            description,
+                            id,
+                            image,
+                            price,
+                            title,
+                            compte
+                        } = product;
+
+                        return (
+                            <div className={styles["container"]} key={id}>
+                                <div className={styles["product-pic"]}>
+                                    <div className={styles["grid-pic"]}>
+                                        <div>
+                                            <img src={image}/>
+                                        </div>
+                                        <div>
+                                            <img src={image}/>
+                                        </div>
+                                        <div>
+                                            <img src={image}/>
+                                        </div>
+                                        <div>
+                                            <img src={image}/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles["product-infos"]}>
+                                    <h2>{title}</h2>
+                                    <p className={styles["product-infos__price"]}>$ {price}</p>
+                                    <p>{description}</p>
+                                    <div className={styles["product-infos__btn"]}>
+                                        <div className={styles["btn-add"]}>
+                                            <div>
+                                                <p onClick={NumberofItems}>-</p>
+                                            </div>
+                                            <div>
+                                                <p>{nombre}</p>
+                                            </div>
+                                            <div>
+                                                <p
+                                                    onClick={(e) => {
+                                                    NumberofItems(e)
+                                                    Compteproduct()
+                                                }}>+</p>
+                                            </div>
+                                        </div>
+
+                                        <div className={styles["btn-add"]}>
+                                            <p>ADD TO CART</p>
+                                        </div>
+                                    </div>
+                                    <div className={styles["reseau"]}>
+                                        <ul>
+                                            <li>
+                                                <i className="fa-regular fa-heart"></i>
+                                            </li>
+                                            <li>|</li>
+                                            <li>
+                                                <i className="fa-regular fa-envelope"></i>
+                                            </li>
+                                            <li>
+                                                <i className="fa-brands fa-facebook-f"></i>
+                                            </li>
+                                            <li>
+                                                <i className="fa-brands fa-instagram"></i>
+                                            </li>
+                                            <li>
+                                                <i className="fa-brands fa-twitter"></i>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-              );
-            })}
-        </div>
-        <div className={styles["see-more"]}>
-          <div className={styles["see-more__items"]}>
-            <ul>
-              <li className={styles[`${items === 1 ? 'li-selected' : ''}`]} onClick={() => ItemsSwitch(1)}>Description</li>
-              <li className={styles[`${items === 2 ? 'li-selected' : ''}`]} onClick={() => ItemsSwitch(2)}>Aditional information</li>
-              <li className={styles[`${items === 3 ? 'li-selected' : ''}`]} onClick={() => ItemsSwitch(3)}>Reviews(0)</li>
-            </ul>
-          </div>
-          <div className={styles["see-more__content"]}>
-            {items === 1 && <Desciption articles={categories} />}
-            {items === 2 && <AddInformations articles={categories} product={data} /> }
-            {items === 3 && <Review articles={categories} />}
+                <div className={styles["see-more"]}>
+                    <div className={styles["see-more__items"]}>
+                        <ul>
+                            <li
+                                className={styles[`${items === 1
+                                    ? 'li-selected'
+                                    : ''}`]}
+                                onClick={() => ItemsSwitch(1)}>Description</li>
+                            <li
+                                className={styles[`${items === 2
+                                    ? 'li-selected'
+                                    : ''}`]}
+                                onClick={() => ItemsSwitch(2)}>Aditional information</li>
+                            <li
+                                className={styles[`${items === 3
+                                    ? 'li-selected'
+                                    : ''}`]}
+                                onClick={() => ItemsSwitch(3)}>Reviews(0)</li>
+                        </ul>
+                    </div>
+                    <div className={styles["see-more__content"]}>
+                        {items === 1 && <Desciption articles={categories}/>}
+                        {items === 2 && <AddInformations articles={categories} product={data}/>}
+                        {items === 3 && <Review articles={categories}/>}
 
-
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 }
 
 export default Products;
