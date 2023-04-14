8f9e570fd9a7adf2f92dc7cea0b3e90e2724f509
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-04-15 01:05:42 +0200

    contact

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5lq_WDIFF3HYQn5_PDwxJQgDukQNqCi0",
+  authDomain: "shoopeapp-f909b.firebaseapp.com",
+  projectId: "shoopeapp-f909b",
+  storageBucket: "shoopeapp-f909b.appspot.com",
+  messagingSenderId: "565898691804",
+  appId: "1:565898691804:web:54d6e5b54c47ecd743a019",
+  measurementId: "G-WCFGKSV2CJ"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebaseapp);
+const db = getFirestore(firebaseapp)
+export default db
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,54 +1,113 @@
-import React from "react";
+import React, {useEffect, useState, useReducer, useRef} from "react";
 import Layout from "../../components/layout/Layout";
 import styles from "./contact.module.scss";
+import db from "../../firebase/config";
+import { collection,getDocs,addDoc } from "firebase/firestore";
+
+//  find all the users in the collection users
+
+      // (async () =>{
+        //     const colRef = collection(db,'users')
+        //     const snapshot = await getDocs(colRef)
+        //     let doc = snapshot.docs.map(doc => doc.data())
+        //     console.log(doc)
+
+        // })()
+
+
+
 
 function Contact(props) {
-  return (
-    <Layout>
-      <div className={styles["contact"]}>
-        <div className={styles["contact__title"]}>
-          <h1>Contact Us</h1>
-          <p>
-            Say Hello send us your thoughts about our products or share <br />
-            your ideas with our Team!
-          </p>
-        </div>
-        <div className={styles['contact__inpt']}>
-            <div className={styles['grid']}>
-           <div>
-            <label>
-                <input placeholder="First name" />
-            </label>
-           </div>
-           <div>
-            <label>
-                <input placeholder="Last name" />
-            </label>
-           </div>
-           <div>
-            <label>
-                <input placeholder="Email" />
-            </label>
-           </div>
-           <div>
-            <label>
-                <input placeholder="Subject" />
-            </label>
-           </div>
-           </div>
-           <div className={styles['message']}>
-            <p>Message</p>
-           <input />
-           </div>
-        
-        </div>
 
-        <div className={styles['contact__btn']}>
-         <button>Send</button>
-        </div>
-      </div>
-    </Layout>
-  );
+    const [user,
+        setuser] = useReducer((state, newState) => ({
+        ...state,
+        ...newState
+    }), {
+        Last_name: '',
+        First_name: '',
+        Email: '',
+        Subject: '',
+        Message: ''
+    });
+
+    const handleOnChange =  (event) => {
+      let champs = event.target.name 
+      const {value}= event.target;
+  
+        console.log(champs,value)
+        setuser({[champs]: value});
+    };
+
+    let all_Input = useRef(null)
+    all_Input.current = []
+    
+    const Handleclick = async () => {
+      const colRef = collection(db,'users')
+      await addDoc(colRef,user)
+
+      all_Input.current.forEach(element => element !== null ? element.value = '' : null);
+
+    }
+
+    const handleInput = (e) =>{
+
+ 
+      if(!all_Input.current.includes(e) && e !== null){
+        all_Input.current.push(e)
+      }
+    }
+    useEffect(() => {
+     console.log(all_Input)
+    })
+
+    return (
+        <Layout>
+            <div className={styles["contact"]}>
+                <div className={styles["contact__title"]}>
+                    <h1>Contact Us</h1>
+                    <p>
+                        Say Hello send us your thoughts about our products or share
+                        <br/>
+                        your ideas with our Team!
+                    </p>
+                </div>
+                <div className={styles['contact__inpt']}>
+                    <div className={styles['grid']}>
+                        <div>
+                            <label>
+                                <input placeholder="First name" ref={handleInput}   name="First_name" onChange={handleOnChange}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="Last name"    ref={handleInput}   name="Last_name" onChange={handleOnChange}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="Email"    ref={handleInput}   name="Email" onChange={handleOnChange}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input placeholder="Subject"    ref={handleInput}   name="Subject" onChange={handleOnChange}/>
+                            </label>
+                        </div>
+                    </div>
+                    <div className={styles['message']}>
+                        <p>Message</p>
+                        <input    ref={handleInput}    name="Message" onChange={handleOnChange}/>
+                    </div>
+
+                </div>
+
+                <div className={styles['contact__btn']}>
+                    <button onClick={Handleclick}>Send</button>
+                </div>
+            </div>
+        </Layout>
+    );
 }
 
 export default Contact;
